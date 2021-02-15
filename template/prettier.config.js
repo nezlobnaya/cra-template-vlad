90405ea0bc5ea29ca0c35c6c7c5b38dfdcf5e4c0
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-02-15 15:15:30 -0500

    template.json updated, dependencies added, prettier, husky added

---
+++ template/prettier.config.js
@@ -0,0 +1,8 @@
+// Some of these are defaults, but let's be explicit for other humans
+module.exports = {
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    printWidth: 80,
+  };
